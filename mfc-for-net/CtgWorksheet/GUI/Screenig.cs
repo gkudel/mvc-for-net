4a5eb83a40af3faaa09cb6f3495742cb4da345d9
gkudel@gmail.com <gkudel@gmail.com@6dc81dcb-d53e-6acd-dc8d-fed7694fc265>
2013-11-28 18:50:05 +0000

    [ViewId]

--- mfc-for-net/CtgWorksheet/GUI/Screenig.cs
+++ mfc-for-net/CtgWorksheet/GUI/Screenig.cs
@@ -12,25 +12,17 @@ namespace MvcForNet.CtgWorksheet.GUI
 {
     public partial class Screenig : UserControl
     {
-        #region Member
-        private int _id = -1;
-        #endregion Member
-
         #region Constructor
         public Screenig(int id)
         {
             InitializeComponent();
-            this._id = id;
 
         }
         #endregion Constructor
 
         #region Properties
         [ViewId("")]
-        public int Id 
-        {
-            get { return _id; }
-        }
+        public int Id { get; private set; }
         #endregion Properties
     }
 }
