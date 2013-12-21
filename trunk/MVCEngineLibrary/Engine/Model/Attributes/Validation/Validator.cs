a3ca8521115fcf6f0a7234becd712d5c66d66a43
gkudel@gmail.com <gkudel@gmail.com@6dc81dcb-d53e-6acd-dc8d-fed7694fc265>
2013-12-21 14:56:52 +0000

    {Missing File}

--- trunk/MVCEngineLibrary/Engine/Model/Attributes/Validation/Validator.cs
+++ trunk/MVCEngineLibrary/Engine/Model/Attributes/Validation/Validator.cs
@@ -7,6 +7,13 @@ namespace MVCEngine.Model.Attributes.Validation
 {
     public abstract class Validator : System.Attribute
     {
+        #region Constructor
+        public Validator()
+        {
+            IfFaildThrowException = true;
+        }
+        #endregion Constructor
+
         #region Properties
         public string ErrrorMessage { get; set; }
         public virtual bool RealTimeValidation { get; set; }
